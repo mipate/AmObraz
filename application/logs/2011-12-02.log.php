be0552891f33ce42348b60d4bb153317016c462e
MonicaIpate <monica.ipate@gmail.com>
2011-12-12 14:09:31 +0200

    Initial upload

---
+++ application/logs/2011-12-02.log.php
@@ -0,0 +1,154 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2011-12-02 14:40:05 +02:00 --- debug: Cache Library initialized
+2011-12-02 14:40:05 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 14:40:05 +02:00 --- debug: Database Library initialized
+2011-12-02 14:40:05 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 14:40:05 +02:00 --- debug: Database Library initialized
+2011-12-02 14:40:05 +02:00 --- debug: Disable magic_quotes_gpc! It is evil and deprecated: http://php.net/magic_quotes
+2011-12-02 14:40:05 +02:00 --- debug: Global GET, POST and COOKIE data sanitized
+2011-12-02 14:40:05 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 14:40:05 +02:00 --- debug: Database Library initialized
+2011-12-02 14:40:05 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 14:40:05 +02:00 --- debug: Database Library initialized
+2011-12-02 15:04:20 +02:00 --- debug: Cache Library initialized
+2011-12-02 15:04:20 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 15:04:20 +02:00 --- debug: Database Library initialized
+2011-12-02 15:04:20 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 15:04:20 +02:00 --- debug: Database Library initialized
+2011-12-02 15:04:20 +02:00 --- debug: Disable magic_quotes_gpc! It is evil and deprecated: http://php.net/magic_quotes
+2011-12-02 15:04:20 +02:00 --- debug: Global GET, POST and COOKIE data sanitized
+2011-12-02 15:04:20 +02:00 --- debug: Encrypt Library initialized
+2011-12-02 15:04:20 +02:00 --- debug: Session Cookie Driver Initialized
+2011-12-02 15:04:20 +02:00 --- debug: Session Library initialized
+2011-12-02 15:04:20 +02:00 --- debug: Auth Library loaded
+2011-12-02 15:04:20 +02:00 --- debug: Cache Library initialized
+2011-12-02 15:04:20 +02:00 --- debug: Cache Library initialized
+2011-12-02 15:04:20 +02:00 --- debug: Auth Library loaded
+2011-12-02 15:04:21 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 15:04:21 +02:00 --- debug: Database Library initialized
+2011-12-02 15:04:23 +02:00 --- debug: Cache Library initialized
+2011-12-02 15:04:23 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 15:04:23 +02:00 --- debug: Database Library initialized
+2011-12-02 15:04:23 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 15:04:23 +02:00 --- debug: Database Library initialized
+2011-12-02 15:04:23 +02:00 --- debug: Disable magic_quotes_gpc! It is evil and deprecated: http://php.net/magic_quotes
+2011-12-02 15:04:23 +02:00 --- debug: Global GET, POST and COOKIE data sanitized
+2011-12-02 15:04:23 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 15:04:23 +02:00 --- debug: Database Library initialized
+2011-12-02 15:04:23 +02:00 --- debug: Cache Library initialized
+2011-12-02 15:04:23 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 15:04:23 +02:00 --- debug: Database Library initialized
+2011-12-02 15:04:23 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 15:04:23 +02:00 --- debug: Database Library initialized
+2011-12-02 15:04:23 +02:00 --- debug: Disable magic_quotes_gpc! It is evil and deprecated: http://php.net/magic_quotes
+2011-12-02 15:04:23 +02:00 --- debug: Global GET, POST and COOKIE data sanitized
+2011-12-02 15:04:23 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 15:04:23 +02:00 --- debug: Database Library initialized
+2011-12-02 15:04:24 +02:00 --- debug: Cache Library initialized
+2011-12-02 15:04:24 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 15:04:24 +02:00 --- debug: Database Library initialized
+2011-12-02 15:04:24 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 15:04:24 +02:00 --- debug: Database Library initialized
+2011-12-02 15:04:24 +02:00 --- debug: Disable magic_quotes_gpc! It is evil and deprecated: http://php.net/magic_quotes
+2011-12-02 15:04:24 +02:00 --- debug: Global GET, POST and COOKIE data sanitized
+2011-12-02 15:04:24 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 15:04:24 +02:00 --- debug: Database Library initialized
+2011-12-02 15:04:24 +02:00 --- debug: Cache Library initialized
+2011-12-02 15:04:24 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 15:04:24 +02:00 --- debug: Database Library initialized
+2011-12-02 15:04:24 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 15:04:24 +02:00 --- debug: Database Library initialized
+2011-12-02 15:04:24 +02:00 --- debug: Disable magic_quotes_gpc! It is evil and deprecated: http://php.net/magic_quotes
+2011-12-02 15:04:24 +02:00 --- debug: Global GET, POST and COOKIE data sanitized
+2011-12-02 15:04:24 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 15:04:24 +02:00 --- debug: Database Library initialized
+2011-12-02 15:04:24 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 15:04:24 +02:00 --- debug: Database Library initialized
+2011-12-02 15:04:25 +02:00 --- debug: Cache Library initialized
+2011-12-02 15:04:25 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 15:04:25 +02:00 --- debug: Database Library initialized
+2011-12-02 15:04:25 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 15:04:25 +02:00 --- debug: Database Library initialized
+2011-12-02 15:04:25 +02:00 --- debug: Disable magic_quotes_gpc! It is evil and deprecated: http://php.net/magic_quotes
+2011-12-02 15:04:25 +02:00 --- debug: Global GET, POST and COOKIE data sanitized
+2011-12-02 15:04:25 +02:00 --- debug: Cache Library initialized
+2011-12-02 15:04:25 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 15:04:25 +02:00 --- debug: Database Library initialized
+2011-12-02 15:04:25 +02:00 --- debug: Cache Library initialized
+2011-12-02 15:04:37 +02:00 --- debug: Cache Library initialized
+2011-12-02 15:04:37 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 15:04:37 +02:00 --- debug: Database Library initialized
+2011-12-02 15:04:37 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 15:04:37 +02:00 --- debug: Database Library initialized
+2011-12-02 15:04:37 +02:00 --- debug: Disable magic_quotes_gpc! It is evil and deprecated: http://php.net/magic_quotes
+2011-12-02 15:04:37 +02:00 --- debug: Global GET, POST and COOKIE data sanitized
+2011-12-02 15:04:37 +02:00 --- debug: Encrypt Library initialized
+2011-12-02 15:04:37 +02:00 --- debug: Session Cookie Driver Initialized
+2011-12-02 15:04:37 +02:00 --- debug: Session Library initialized
+2011-12-02 15:04:37 +02:00 --- debug: Auth Library loaded
+2011-12-02 15:04:37 +02:00 --- debug: Cache Library initialized
+2011-12-02 15:04:37 +02:00 --- debug: Cache Library initialized
+2011-12-02 15:04:37 +02:00 --- debug: Auth Library loaded
+2011-12-02 15:04:38 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 15:04:38 +02:00 --- debug: Database Library initialized
+2011-12-02 16:01:29 +02:00 --- debug: Cache Library initialized
+2011-12-02 16:01:29 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 16:01:29 +02:00 --- debug: Database Library initialized
+2011-12-02 16:01:29 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 16:01:29 +02:00 --- debug: Database Library initialized
+2011-12-02 16:01:29 +02:00 --- debug: Disable magic_quotes_gpc! It is evil and deprecated: http://php.net/magic_quotes
+2011-12-02 16:01:29 +02:00 --- debug: Global GET, POST and COOKIE data sanitized
+2011-12-02 16:01:29 +02:00 --- debug: Encrypt Library initialized
+2011-12-02 16:01:29 +02:00 --- debug: Session Cookie Driver Initialized
+2011-12-02 16:01:29 +02:00 --- debug: Session Library initialized
+2011-12-02 16:01:29 +02:00 --- debug: Auth Library loaded
+2011-12-02 16:01:29 +02:00 --- debug: Cache Library initialized
+2011-12-02 16:01:29 +02:00 --- debug: Cache Library initialized
+2011-12-02 16:01:29 +02:00 --- debug: Auth Library loaded
+2011-12-02 16:01:30 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 16:01:30 +02:00 --- debug: Database Library initialized
+2011-12-02 16:01:34 +02:00 --- debug: Cache Library initialized
+2011-12-02 16:01:34 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 16:01:34 +02:00 --- debug: Database Library initialized
+2011-12-02 16:01:34 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 16:01:34 +02:00 --- debug: Database Library initialized
+2011-12-02 16:01:34 +02:00 --- debug: Disable magic_quotes_gpc! It is evil and deprecated: http://php.net/magic_quotes
+2011-12-02 16:01:34 +02:00 --- debug: Global GET, POST and COOKIE data sanitized
+2011-12-02 16:01:35 +02:00 --- debug: Cache Library initialized
+2011-12-02 16:01:35 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 16:01:35 +02:00 --- debug: Database Library initialized
+2011-12-02 16:01:35 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 16:01:35 +02:00 --- debug: Database Library initialized
+2011-12-02 16:01:35 +02:00 --- debug: Disable magic_quotes_gpc! It is evil and deprecated: http://php.net/magic_quotes
+2011-12-02 16:01:35 +02:00 --- debug: Global GET, POST and COOKIE data sanitized
+2011-12-02 16:01:35 +02:00 --- debug: Cache Library initialized
+2011-12-02 16:01:35 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 16:01:35 +02:00 --- debug: Database Library initialized
+2011-12-02 16:01:35 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 16:01:35 +02:00 --- debug: Database Library initialized
+2011-12-02 16:01:35 +02:00 --- debug: Disable magic_quotes_gpc! It is evil and deprecated: http://php.net/magic_quotes
+2011-12-02 16:01:35 +02:00 --- debug: Global GET, POST and COOKIE data sanitized
+2011-12-02 16:01:35 +02:00 --- debug: Cache Library initialized
+2011-12-02 16:01:35 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 16:01:35 +02:00 --- debug: Database Library initialized
+2011-12-02 16:01:35 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 16:01:35 +02:00 --- debug: Database Library initialized
+2011-12-02 16:01:35 +02:00 --- debug: Disable magic_quotes_gpc! It is evil and deprecated: http://php.net/magic_quotes
+2011-12-02 16:01:35 +02:00 --- debug: Global GET, POST and COOKIE data sanitized
+2011-12-02 16:01:36 +02:00 --- debug: Cache Library initialized
+2011-12-02 16:01:36 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 16:01:36 +02:00 --- debug: Database Library initialized
+2011-12-02 16:01:36 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 16:01:36 +02:00 --- debug: Database Library initialized
+2011-12-02 16:01:36 +02:00 --- debug: Disable magic_quotes_gpc! It is evil and deprecated: http://php.net/magic_quotes
+2011-12-02 16:01:36 +02:00 --- debug: Global GET, POST and COOKIE data sanitized
+2011-12-02 16:01:36 +02:00 --- debug: Encrypt Library initialized
+2011-12-02 16:01:36 +02:00 --- debug: Session Cookie Driver Initialized
+2011-12-02 16:01:36 +02:00 --- debug: Session Library initialized
+2011-12-02 16:01:36 +02:00 --- debug: Auth Library loaded
+2011-12-02 16:01:36 +02:00 --- debug: Cache Library initialized
+2011-12-02 16:01:36 +02:00 --- debug: Cache Library initialized
+2011-12-02 16:01:36 +02:00 --- debug: Auth Library loaded
+2011-12-02 16:01:36 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-02 16:01:36 +02:00 --- debug: Database Library initialized
---
+++ application/logs/2011-12-03.log.php
@@ -0,0 +1,58 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2011-12-03 20:26:34 +02:00 --- debug: Cache Library initialized
+2011-12-03 20:26:35 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-03 20:26:35 +02:00 --- debug: Database Library initialized
+2011-12-03 20:26:35 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-03 20:26:35 +02:00 --- debug: Database Library initialized
+2011-12-03 20:26:35 +02:00 --- debug: Disable magic_quotes_gpc! It is evil and deprecated: http://php.net/magic_quotes
+2011-12-03 20:26:35 +02:00 --- debug: Global GET, POST and COOKIE data sanitized
+2011-12-03 20:26:35 +02:00 --- debug: Encrypt Library initialized
+2011-12-03 20:26:35 +02:00 --- debug: Session Cookie Driver Initialized
+2011-12-03 20:26:35 +02:00 --- debug: Session Library initialized
+2011-12-03 20:26:35 +02:00 --- debug: Auth Library loaded
+2011-12-03 20:26:35 +02:00 --- debug: Cache Library initialized
+2011-12-03 20:26:35 +02:00 --- debug: Cache Library initialized
+2011-12-03 20:26:35 +02:00 --- debug: Auth Library loaded
+2011-12-03 20:26:36 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-03 20:26:36 +02:00 --- debug: Database Library initialized
+2011-12-03 20:26:39 +02:00 --- debug: Cache Library initialized
+2011-12-03 20:26:39 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-03 20:26:39 +02:00 --- debug: Database Library initialized
+2011-12-03 20:26:39 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-03 20:26:39 +02:00 --- debug: Database Library initialized
+2011-12-03 20:26:39 +02:00 --- debug: Disable magic_quotes_gpc! It is evil and deprecated: http://php.net/magic_quotes
+2011-12-03 20:26:39 +02:00 --- debug: Global GET, POST and COOKIE data sanitized
+2011-12-03 20:26:39 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-03 20:26:39 +02:00 --- debug: Database Library initialized
+2011-12-03 20:26:39 +02:00 --- debug: Cache Library initialized
+2011-12-03 20:26:39 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-03 20:26:39 +02:00 --- debug: Database Library initialized
+2011-12-03 20:26:39 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-03 20:26:39 +02:00 --- debug: Database Library initialized
+2011-12-03 20:26:39 +02:00 --- debug: Disable magic_quotes_gpc! It is evil and deprecated: http://php.net/magic_quotes
+2011-12-03 20:26:39 +02:00 --- debug: Global GET, POST and COOKIE data sanitized
+2011-12-03 20:26:39 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-03 20:26:39 +02:00 --- debug: Database Library initialized
+2011-12-03 20:26:39 +02:00 --- debug: Cache Library initialized
+2011-12-03 20:26:39 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-03 20:26:39 +02:00 --- debug: Database Library initialized
+2011-12-03 20:26:39 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-03 20:26:39 +02:00 --- debug: Database Library initialized
+2011-12-03 20:26:39 +02:00 --- debug: Disable magic_quotes_gpc! It is evil and deprecated: http://php.net/magic_quotes
+2011-12-03 20:26:39 +02:00 --- debug: Global GET, POST and COOKIE data sanitized
+2011-12-03 20:26:39 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-03 20:26:39 +02:00 --- debug: Database Library initialized
+2011-12-03 20:26:39 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-03 20:26:39 +02:00 --- debug: Database Library initialized
+2011-12-03 20:26:39 +02:00 --- debug: Cache Library initialized
+2011-12-03 20:26:39 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-03 20:26:39 +02:00 --- debug: Database Library initialized
+2011-12-03 20:26:39 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-03 20:26:39 +02:00 --- debug: Database Library initialized
+2011-12-03 20:26:39 +02:00 --- debug: Disable magic_quotes_gpc! It is evil and deprecated: http://php.net/magic_quotes
+2011-12-03 20:26:39 +02:00 --- debug: Global GET, POST and COOKIE data sanitized
+2011-12-03 20:26:39 +02:00 --- debug: Cache Library initialized
+2011-12-03 20:26:39 +02:00 --- debug: MySQL Database Driver Initialized
+2011-12-03 20:26:39 +02:00 --- debug: Database Library initialized
+2011-12-03 20:26:39 +02:00 --- debug: Cache Library initialized
